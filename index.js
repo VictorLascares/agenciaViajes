4d2ab1855972b144f1f32fb9de75c476beebbcf2
VictorLascares <vmlascares@gmail.com>
2022-11-04 11:30:10 -0600

    Creando footer y mostrando año dinamico con middlewares

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set("view engine", "pug");
 
+// Obtener el año actual
+app.use((req, res, next) => {
+  const year = new Date();
+
+  res.locals.currentYear = year.getFullYear();
+  return next();
+});
+
 // Definir la carpeta publica
 app.use(express.static("public"));
 
 // Agregar router
 app.use("/", router);
-
 
 app.listen(port, () => {
   console.log(`El servidor es funcionando en el puerto ${port}`);
